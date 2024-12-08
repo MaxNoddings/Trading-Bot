d7203c7679843243a674a72fac30d24c9cda9536
Max Noddings <mnodd4@gmail.com>
2024-12-08 16:57:09 -0500

    filled out shell program

--- compare_Pairs.sh
+++ compare_Pairs.sh
@@ -2,4 +2,16 @@
 
 python3 pairs_coint_corr_test.py KO PEP
 echo "Next..."
+python3 pairs_coint_corr_test.py XOM CVX
+echo "Next..."
+python3 pairs_coint_corr_test.py PG CL
+echo "Next..."
+python3 pairs_coint_corr_test.py V MA
+echo "Next..."
+python3 pairs_coint_corr_test.py JPM BAC
+echo "Next..."
+python3 pairs_coint_corr_test.py F GM
+echo "Next..."
+python3 pairs_coint_corr_test.py AAL DAL
+echo "Next..."
 python3 pairs_coint_corr_test.py MSFT AAPL
